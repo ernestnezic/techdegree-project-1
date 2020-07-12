f4c56cdb0fa736d366c2561c4354587e113579dd
Ernest Nezic <ernest.nezic@gmail.com>
2020-07-12 13:27:38 +0200

    Filled 'quote' array with quote objects

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -13,19 +13,39 @@ project 1 - A Random Quote Generator
 
 let quotes = [
   {
-
+    quote = "Be yourself; everyone else is already taken.",
+    source = "Oscar Wilde"
   },
   {
-
+    quote = "Without music, life would be a mistake.Without music, life would be a mistake.",
+    source = "Friedrich Nietzsche",
+    citation = "Twilight of the Idols",
+    year = "1889."
   },
   {
-
+    quote = "Life isn't about finding yourself. Life is about creating yourself.",
+    source = "Dr. Seuss",
+    citation = "Happy Birthday to You"
   },
   {
-
+    quote = "Logic will get you from A to Z; imagination will get you everywhere.",
+    source = "Albert Einstein"
   },
   {
-    
+    quote = "Some infinities are bigger than other infinities.",
+    source = "John Green",
+    citation = "The Fault in Our Stars",
+    year = "2012."
+  },
+  {
+    quote = "The trouble with having an open mind, of course, is that people will insist on coming along and trying to put things in it.",
+    source = "Terry Pratchett",
+    citation = "Diggers"
+  },
+  {
+    quote = "We believe in ordinary acts of bravery, in the courage that drives one person to stand up for another.",
+    source = "Veronica Roth",
+    citation = "Divergent"
   }
 
  ];
@@ -47,5 +67,5 @@ let quotes = [
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
-console.log('test');
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+console.log("test");
+document.getElementById("load-quote").addEventListener("click", printQuote, false);
